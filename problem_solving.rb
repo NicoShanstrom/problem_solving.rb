0486cf57192b533b17d715c08c69186e324dddea
Nico Shanstrom <148294962+NicoShanstrom@users.noreply.github.com>
2023-10-28 15:27:11 -1000

    Add files via upload

---
+++ problem_solving.rb
@@ -0,0 +1,36 @@
+steps_to_plant_a_tree = ["Dig a hole", "plant tree", "water"]
+p steps_to_plant_a_tree.length.even?
+#What do you like about this process?
+    #It is always good to have a method to the madness. Trust the process. With a plan, it is easier to achieve the outcome. Without a plan, I am all over the place. 
+#What might make this process challenging?
+    #It could be more challenging if one does not break down the process into basic pieces, it may be hard to google a long block of information. If you are not specific, you'll get too much information.
+#Are there any steps you would add or modify?
+    # Take a break if you are struggling. Being frustrated and in a rush does not help.
+
+
+def current_time = "14"
+   if current_time (4..11)
+    p "Good morning"
+   elsif current_time (12..16)
+    p "Good afternoon"
+   elsif current_time (17..20)
+    p "Good evening"
+   elsif current_time (21..24)
+    p "Good night"
+   elsif current_time (0..3)
+    p "Night owl"
+   else current time 
+    p "What planet are you on?"
+   end
+
+   current_time
+
+   def create_phone_number(5551234567):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*5551234567)
+   end
+   
+   def phone_numbers(arr)
+    arr.join('').insert(-4, '-').insert(-8, '-')
+  end 
+  puts phone_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) 
+   
